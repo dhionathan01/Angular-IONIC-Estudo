bdb2404a9236929ebf8d6f21aac7cd0838655332
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-12-27 23:31:43 -0300

    feat: :sparkles: Criando um método
    Criando um método e o chamando quando o evento "input" for acionado

--- Projects/ionicApp/src/app/home/home.page.ts
+++ Projects/ionicApp/src/app/home/home.page.ts
@@ -10,4 +10,8 @@ export class HomePage {
   public titulo: string = "App Com Angular";
   public imagemRandomica: string = "https://source.unsplash.com/random/800x600";
   public imagemLocal: string = "../assets/img/icone-celular.png";
+
+  public atualizar(): void {
+    this.titulo = "Texto alterado";
+  }
 }
