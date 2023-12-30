7df56d7593be7c54e946ab1cb02f41b42c372796
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-12-29 21:12:11 -0300

    feat: :sparkles: Criando navegação com onclick

--- Projects/ionicApp/src/app/home/home.page.ts
+++ Projects/ionicApp/src/app/home/home.page.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   public titulo: string = "App Com Angular";
   public imagemRandomica: string = "https://source.unsplash.com/random/800x600";
   public imagemLocal: string = "../assets/img/icone-celular.png";
 
+  constructor(private navegacao: NavController) {
+
+  }
+  ngOnInit(){
+
+  }
   public atualizar(): void {
     this.titulo = "Texto alterado";
   }
   public acao(): void {
     this.titulo = "Botão clicado";
+  }
+
+  public abrirLista(){
+    this.navegacao.navigateForward('lista');
+  }
+  public abrirInformacoes() {
+    this.navegacao.navigateForward('informacoes');
+
   }
 }
